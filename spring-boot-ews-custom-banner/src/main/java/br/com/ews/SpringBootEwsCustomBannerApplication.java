67f72988d05af5b018ba35a3fe64d65041b8ed25
Eduardo Wallace <edubossa@gmail.com>
2016-02-04 05:38:07 -0200

    commit inicial

---
+++ spring-boot-ews-custom-banner/src/main/java/br/com/ews/SpringBootEwsCustomBannerApplication.java
@@ -0,0 +1,12 @@
+package br.com.ews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEwsCustomBannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEwsCustomBannerApplication.class, args);
+	}
+}
